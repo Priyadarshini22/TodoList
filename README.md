70b5bd4325500160fa5af9b31eece0e7795da8af
Priyadarshani <priyadarshani@apicalsolutions.com>
2025-03-21 19:48:30 +0530

    screenshot added

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+
+Check image.png screenshot
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [task, setTask] = useState({
+    id: 0,
+    name: "",
+    completed: false,
+    isEditable: false,
+  });
+  const [tasks, setTasks] = useState([]);
+  const [term, setTerm] = useState("");
+  const [filter, setFilter] = useState("All");
+  const [filteredTask, setFilteredTask] = useState(tasks);
+
+  useEffect(() => {
+    filterCheck();
+}, [term, filter, tasks]);
+
+const filterCheck = () => {
+  let filtered = tasks;
+  if (filter === "Completed") {
+    filtered = tasks.filter((item) => item.completed);
+  } else if (filter === "InProgress") {
+    filtered = tasks.filter((item) => !item.completed);
+  }
+  if (term !== "") {
+    filtered = filtered.filter((item) => item.name.toLowerCase().includes(term.toLowerCase()));
+  }
+  setFilteredTask(filtered);
+};
+
+
+  const editName = (name, id) => {
+    setTasks(tasks.map((c) => {
+      if (c.id === id) {
+        return { ...c, name: name };
+      }
+      return c;
+    }));
+  };
+
+  const addTask = () => {
+    setTasks([...tasks, { ...task, id: tasks.length + 1 }]);
+  };
+
+  const editTask = (id) => {
+    setTasks(tasks.map((item) => {
+      if (item.id === id) {
+        return { ...item, isEditable: true };
+      }
+      return item;
+    }));
+  };
+
+  const handleCompleted = (id) => {
+    setTasks(tasks.map((item) => {
+      if (item.id === id) {
+        return { ...item, completed: true, isEditable:false };
+      }
+      return item;
+    }));
+  };
+
+  const removeTask = (id) => {
+    setTasks(tasks.filter((item) => item.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="search">
+        <input type="text" onChange={(e) => setTerm(e.target.value)} />
+        <select name="filters" onChange={(e) => setFilter(e.target.value)}>
+          <option value="All">All</option>
+          <option value="Completed">Completed</option>
+          <option value="InProgress">In Progress</option>
+        </select>
+        <button onClick={() => addTask(task)} id="add">Add</button>
+      </div>
+      {filteredTask.map((item, index) =>
+        <div key={index} className="task-card">
+          <div className="task-content">
+            {!item.isEditable ? <p>{item.name}</p> :
+              <input type="text" value={item.name} onChange={(e) => editName(e.target.value, item.id)} />}
+          </div>
+          <div className="task-actions">
+            <button onClick={() => editTask(item.id)}>Edit</button>
+            <button onClick={() => handleCompleted(item.id)}>Done</button>
+            <button onClick={() => removeTask(item.id)}>X</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
